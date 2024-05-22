a6f8ad341414f3b0b2cf1a1e49df90f54df1e67f
Gennadii <Gennadii_s@inbox.ru>
2024-05-23 04:47:53 +1000

    update code

--- test_app.py
+++ test_app.py
@@ -10,6 +10,7 @@ def app():
     app.config['TESTING'] = True
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
     with app.app_context():
+        db.drop_all()
         db.create_all()
         yield app
 
@@ -21,13 +22,14 @@ def client(app):
 
 @pytest.fixture
 def init_database():
-    db.create_all()
     task1 = Task(title="Task 1", description="Description 1")
     task2 = Task(title="Task 2", description="Description 2")
     db.session.add(task1)
     db.session.add(task2)
     db.session.commit()
     yield db
+    db.session.remove()
+    db.drop_all()
 
 
 def test_get_tasks(client, init_database):
