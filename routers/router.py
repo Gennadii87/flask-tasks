501b5c60be763d19e8d216e16b77882544ebc5ac
Gennadii87 <Gennadii_s@inbox.ru>
2024-05-23 16:39:41 +1000

    upadate code

--- routers/router.py
+++ routers/router.py
@@ -1,6 +1,5 @@
-from flask import request, jsonify
+from flask import request, make_response
 from flask_restx import Resource, Namespace, fields, Api
-
 from database.schemas import TaskCreateSchema, TaskUpdateSchema, TaskSchema, TaskDeleteSchema
 from pydantic import ValidationError
 from database.service import get_task_list, get_task_id, create_task_db, update_task_db, delete_task_db
@@ -39,15 +38,18 @@ task_delete_model = api.model('TaskDeleteSchema', {
 
 @router_api.route('/')
 class Tasks(Resource):
-    @router_api.doc(description="Get list task")
+    @router_api.doc(description="Get list task", order=1)
     @router_api.response(200, 'Task list', task_model)
     def get(self):
         """Получение списка задач"""
         tasks = get_task_list()
         task_schemas = [TaskSchema.from_orm(task).dict() for task in tasks]
-        return jsonify(task_schemas)
+        response = make_response(task_schemas)
+        response.status_code = 200
+        return response
 
-    @router_api.doc(description="Create a new task")
+
+    @router_api.doc(description="Create a new task", order=3)
     @router_api.expect(task_create_model)
     @router_api.response(204, 'Task add', task_model)
     def post(self):
@@ -57,49 +59,55 @@ class Tasks(Resource):
             task_data = TaskCreateSchema(**data)
             new_task = create_task_db(task_data)
             task_schemas = TaskSchema.from_orm(new_task).dict()
-            return jsonify(task_schemas)
+            response = make_response(task_schemas)
+            response.status_code = 201
+            return response
         except ValidationError as e:
-            return jsonify(e.errors())
+            return e.errors(), 400
 
 
 @router_api.route('/<int:id>/')
 class Task(Resource):
-    @router_api.doc(description="Get task by ID")
+    @router_api.doc(description="Get task by ID", order=2)
     @router_api.response(200, 'Task to id', task_model)
     def get(self, id):
         """Получение конкретной задачи"""
         task = get_task_id(id)
         if task:
             task_schema = TaskSchema.from_orm(task).dict()
-            return jsonify(task_schema)
+            response = make_response(task_schema)
+            response.status_code = 200
+            return response
         else:
-            return jsonify({'message': 'Task not found'})
+            return {'message': 'Task not found'}, 404
 
-    @router_api.doc(description="Update a specific task by ID")
+    @router_api.doc(description="Update a specific task by ID", order=4)
     @router_api.expect(task_update_model)
     @router_api.response(200, 'Task update')
     def put(self, id):
         """Обновление задачи"""
         task = get_task_id(id)
         if not task:
-            return jsonify({'message': 'Task not found'})
+            return {'message': 'Task not found'}, 404
 
         try:
             data = request.get_json()
             task_data = TaskUpdateSchema(**data)
             updated_task = update_task_db(task, task_data)
             task_schema = TaskSchema.from_orm(updated_task).dict()
-            return jsonify(task_schema)
+            response = make_response(task_schema)
+            response.status_code = 200
+            return response
         except ValidationError as e:
-            return jsonify(e.errors())
+            return e.errors(), 400
 
-    @router_api.doc(description="Delete task by ID")
+    @router_api.doc(description="Delete task by ID", order=5)
     @router_api.response(204, 'Task deleted', task_delete_model)
     def delete(self, id):
         """Удаление задачи"""
         task = get_task_id(id)
         if task:
             delete_task_db(task)
-            return jsonify({'message': 'Task deleted successfully'})
+            return {'message': 'Task deleted successfully'}, 200
         else:
-            return jsonify({'message': 'Task not found'})
+            return {'message': 'Task not found'}, 404
