c726759bc886ac13c288522134b4c75ccfd20495
Gennadii87 <Gennadii_s@inbox.ru>
2024-05-25 07:33:38 +1000

    update code

--- config/config.py
+++ config/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from flask import Flask
 
 load_dotenv()
 
@@ -10,4 +11,19 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 
 
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = 'sqlite:///sqlite.db'
+
+TEST_DATABASE_URL = 'sqlite:///test.db'
+
+
+class ConfigApp:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    def __init__(self, cls_config=None, db=None):
+        self.app = Flask(__name__)
+        self.app.config.from_object(cls_config)
+        db.init_app(self.app)
+
+    def get_app(self):
+        return self.app
