c4f302d4f66f8824ee961d14e36ff06f8e0bc85a
Gennadii87 <Gennadii_s@inbox.ru>
2024-05-26 04:19:28 +1000

    update code

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
+import os
 from flask_migrate import Migrate
-from config.config import ConfigApp
+from config.config import ConfigApp, LoggerConfig
 from database.database import BaseWork
 from database.models import db
 from routers.router import router_api, api
 
-
 app_config = ConfigApp(BaseWork, db)
 app = app_config.get_app()
+
 migrate = Migrate(app, db)
 
-# если убрать, создавать и применять миграции в ручную,
+log_config = LoggerConfig('INFO')
+
+
+def print_config(app_):
+    """Печать конфигурации приложения в режиме отладки."""
+    # if app.config['DEBUG']:
+    for key, value in app_.config.items():
+        print(f"{key}: {value}")
+
+
+# создавать и применять миграции в ручную
 # flask db init, flask db migrate -m "Initial migration",  flask db upgrade
 with app.app_context():
+    # db.drop_all()
     db.create_all()
 
 
@@ -19,4 +31,6 @@ api.init_app(app)
 api.add_namespace(router_api)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+    if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
+        print_config(app)
+    app.run(port=4000)
