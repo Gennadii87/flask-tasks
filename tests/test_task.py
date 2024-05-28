5de984effcb0522c3dbcc0a72cb14ae06a2e2eea
Gennadii87 <Gennadii_s@inbox.ru>
2024-05-28 20:36:06 +1000

    update test

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,10 +1,12 @@
 from fixture import client, init_database, app_test
+from routers.router import url_prefix
 
+url = url_prefix
 test = app_test
 
 
 def test_get_tasks(client, init_database):
-    response = client.get('/tasks/')
+    response = client.get(f'{url}/')
     assert response.status_code == 200
     tasks = response.get_json()
     assert len(tasks) == 2
@@ -12,8 +14,8 @@ def test_get_tasks(client, init_database):
     assert tasks[1]['title'] == 'Task 2'
 
 
-def test_get_task(client, init_database):
-    response = client.get('/tasks/1/')
+def test_get_task_id(client, init_database):
+    response = client.get(f'{url}/1/')
     assert response.status_code == 200
     task = response.get_json()
     assert task['title'] == 'Task 1'
@@ -22,7 +24,7 @@ def test_get_task(client, init_database):
 
 
 def test_get_task_not_found(client):
-    response = client.get('/tasks/999/')
+    response = client.get(f'{url}/999/')
     assert response.status_code == 404
     data = response.get_json()
     assert data['message'] == 'Task not found'
@@ -30,7 +32,7 @@ def test_get_task_not_found(client):
 
 def test_create_task(client):
     new_task = {"title": "New Task", "description": "New Description"}
-    response = client.post('/tasks/', json=new_task)
+    response = client.post(f'{url}/', json=new_task)
     assert response.status_code == 201
     task = response.get_json()
     assert task['title'] == 'New Task'
@@ -40,13 +42,13 @@ def test_create_task(client):
 
 def test_create_task_validation_error(client):
     invalid_task = {"title": ""}
-    response = client.post('/tasks/', json=invalid_task)
+    response = client.post(f'{url}/', json=invalid_task)
     assert response.status_code == 400
 
 
 def test_update_task(client, init_database):
     update_data = {"title": "Updated Task", "description": "Updated Description"}
-    response = client.put('/tasks/1/', json=update_data)
+    response = client.put(f'{url}/1/', json=update_data)
     assert response.status_code == 200
     task = response.get_json()
     assert task['title'] == 'Updated Task'
@@ -57,14 +59,14 @@ def test_update_task(client, init_database):
 
 def test_update_task_not_found(client):
     update_data = {"title": "Updated Task"}
-    response = client.put('/tasks/999/', json=update_data)
+    response = client.put(f'{url}/999/', json=update_data)
     assert response.status_code == 404
     data = response.get_json()
     assert data['message'] == 'Task not found'
 
 
 def test_delete_task(client, init_database):
-    response = client.delete('/tasks/1/')
+    response = client.delete(f'{url}/1/')
     assert response.status_code == 200
     data = response.get_json()
     title = data.get('task', {}).get("title")
@@ -74,7 +76,7 @@ def test_delete_task(client, init_database):
 
 
 def test_delete_task_not_found(client):
-    response = client.delete('/tasks/999/')
+    response = client.delete(f'{url}/999/')
     assert response.status_code == 404
     data = response.get_json()
     assert data['message'] == 'Task not found'
